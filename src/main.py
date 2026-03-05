c56d87b0fe6ff6774485ae347ef5e1d5a66bf341
kevinjin12 <kevinjin345@gmail.com>
2026-03-04 23:08:56 -0500

    implemented lru

--- src/main.py
+++ src/main.py
@@ -1,25 +1,44 @@
 from collections import deque
 
 def fifo(k, requests):
-    cache = set()
-    order = deque()
+    cache = deque()
     misses = 0
 
     for r in requests:
+        # cache hit
         if r in cache:
             continue
 
+        # cache miss
         misses += 1
 
         if len(cache) == k:
-            evicted = order.popleft()
-            cache.remove(evicted)
+            cache.popleft()
 
-        cache.add(r)
-        order.append(r)
+        cache.append(r)
 
     return misses
 
+def lru(k, requests):
+    cache = deque()
+    misses = 0
+
+    for r in requests:
+        # cache hit
+        if r in cache:
+            cache.remove(r)
+            cache.append(r)
+
+        # cache miss
+        else:            
+            misses += 1
+
+            if len(cache) == k:
+                cache.popleft()
+
+            cache.append(r)
+
+    return misses
 
 def main():
     file_path = "tests/test1.in"
@@ -29,8 +48,10 @@ def main():
         requests = list(map(int, f.readline().split()))
 
     fifo_misses = fifo(k, requests)
+    lru_misses = lru(k, requests)
 
     print(f"FIFO  : {fifo_misses}")
+    print(f"LRU   : {lru_misses}")
 
 
 if __name__ == "__main__":
