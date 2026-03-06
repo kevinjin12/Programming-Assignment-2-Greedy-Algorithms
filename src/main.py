19a2119f8804e2818979f06a0cd3878545e03308
Keerat Kohli <kkeerat012@gmail.com>
2026-03-05 22:20:33 -0500

    Optimzied fifo and lru implementations to be faster and added simple_test input/output files to test correctness

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from collections import deque
 
 def fifo(k, requests):
-    cache = deque()
+    cache = set()
+    first_in = deque()
     misses = 0
 
     for r in requests:
@@ -13,35 +14,38 @@ def fifo(k, requests):
         misses += 1
 
         if len(cache) == k:
-            cache.popleft()
+            elemented_evicted = first_in.popleft()
+            cache.remove(elemented_evicted)
 
-        cache.append(r)
+        first_in.append(r)
+        cache.add(r)
 
     return misses
 
 def lru(k, requests):
-    cache = deque()
+    cache = {}
     misses = 0
 
     for r in requests:
         # cache hit
         if r in cache:
-            cache.remove(r)
-            cache.append(r)
+            cache.pop(r)
+            cache[r] = 1
 
         # cache miss
         else:            
             misses += 1
 
             if len(cache) == k:
-                cache.popleft()
+                cache.pop(next(iter(cache))) # Get first inserted key (lru element) and remove it
 
-            cache.append(r)
+            cache[r] = 1
 
     return misses
 
 def main():
-    file_path = "tests/test1.in"
+    file_path = "../tests/simple_test.in"
+    output_path = "../tests/simple_test.out"
 
     with open(file_path, "r") as f:
         k, m = map(int, f.readline().split())
@@ -50,9 +54,12 @@ def main():
     fifo_misses = fifo(k, requests)
     lru_misses = lru(k, requests)
 
+    with open(output_path, "w") as f:
+        f.write(f"FIFO  : {fifo_misses}\n")
+        f.write(f"LRU   : {lru_misses}\n")
+
     print(f"FIFO  : {fifo_misses}")
     print(f"LRU   : {lru_misses}")
-
 
 if __name__ == "__main__":
     main()
