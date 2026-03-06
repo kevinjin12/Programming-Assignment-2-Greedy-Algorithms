3f7057a9221758a7a97b52ead221d337286cc1e4
Keerat Kohli <kkeerat012@gmail.com>
2026-03-06 00:57:53 -0500

    Added implementation for OPTFF

--- src/main.py
+++ src/main.py
@@ -43,6 +43,40 @@ def lru(k, requests):
 
     return misses
 
+def optff(k, requests):
+    next_indx = {}
+
+    for indx, r in enumerate(requests):
+        if r not in next_indx:
+            next_indx[r] = deque()
+        next_indx[r].append(indx)
+
+    cache = set()
+    misses = 0
+    for r in requests:
+
+        next_indx[r].popleft()
+
+        if r in cache:
+            continue
+
+        misses += 1
+        if len(cache) == k:
+            req_with_furthest_indx = -1
+            furthest_indx = -1
+
+            for elem in cache:
+                elem_next_indx = next_indx[elem][0] if next_indx[elem] else len(requests)
+                if elem_next_indx > furthest_indx:
+                    req_with_furthest_indx = elem
+                    furthest_indx = elem_next_indx
+            
+            cache.remove(req_with_furthest_indx)
+        
+        cache.add(r)
+    
+    return misses
+    
 def main():
     file_path = "../tests/simple_test.in"
     output_path = "../tests/simple_test.out"
@@ -53,13 +87,16 @@ def main():
 
     fifo_misses = fifo(k, requests)
     lru_misses = lru(k, requests)
+    optff_misses = optff(k, requests)
 
     with open(output_path, "w") as f:
         f.write(f"FIFO  : {fifo_misses}\n")
         f.write(f"LRU   : {lru_misses}\n")
+        f.write(f"OPTFF : {optff_misses}")
 
     print(f"FIFO  : {fifo_misses}")
     print(f"LRU   : {lru_misses}")
+    print(f"OPTFF : {optff_misses}")
 
 if __name__ == "__main__":
     main()
